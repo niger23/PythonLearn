98bc62a830fc3484784fecba745a213df60b7f6a
Fingerlink <niger-23@yandex.ru>
2022-10-07 15:53:22 +0700

    added new Seminar001

--- Seminars/Seminar001/seminar001.py
+++ Seminars/Seminar001/seminar001.py
@@ -70,10 +70,22 @@
 # except:
 #     print('Введите целое число')
 
+
+# try:
+#     x = float(input('enter x:'))
+#     if int(x) - x == 0:
+#         print('no')    
+#     else:
+#         print(int(x * 10 % 10))
+# except:
+#     print('enter numbers')
+
+
+
 # 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30.
 
 def multiple(num):
-    if ((num % 5 == 0 and num % 10 == 0) or num % 15 == 0) and num % 30 != 0:
+    if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
         print("Число удовлетворяет данным условиям")
     else:
         print("Число не удовлетворяет данным условиям")
